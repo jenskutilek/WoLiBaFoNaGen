793acb83b0bed92727d65b20f94e75e4c8a8e769
Jens Kutilek <webmail@kutilek.de>
2022-10-11 18:53:24 +0200

    Load defaults

--- WoLiBaFoNaGen.glyphsPlugin/Contents/Resources/plugin.py
+++ WoLiBaFoNaGen.glyphsPlugin/Contents/Resources/plugin.py
@@ -1,8 +1,10 @@
 import objc
 
 from AppKit import NSMenuItem
-from GlyphsApp import EDIT_MENU, Glyphs
+from GlyphsApp import DOCUMENTOPENED, WINDOW_MENU, Glyphs
 from GlyphsApp.plugins import GeneralPlugin
+
+key = "de.kutilek.glyphs.WoLiBaFoNaGen.%s"
 
 
 class WoLiBaFoNaGen(GeneralPlugin):
@@ -17,7 +19,21 @@ class WoLiBaFoNaGen(GeneralPlugin):
     @objc.python_method
     def start(self):
         newMenuItem = NSMenuItem(self.name, self.showWindow_)
-        Glyphs.menu[EDIT_MENU].append(newMenuItem)
+        Glyphs.menu[WINDOW_MENU].append(newMenuItem)
+        self.load_defaults()
+
+    @objc.python_method
+    def load_defaults(self):
+        self.word_list_name = Glyphs.defaults[key % "word_list"]
+        self.min_length = Glyphs.defaults[key % "min_length"]
+        self.max_length = Glyphs.defaults[key % "max_length"]
+        self.ideal_length = Glyphs.defaults[key % "ideal_length"]
+        self.length_influence = Glyphs.defaults[key % "length_influence"]
+        self.first_letters = Glyphs.defaults[key % "first_letters"]
+        self.other_letters = Glyphs.defaults[key % "other_letters"]
+        self.prefix = Glyphs.defaults[key % "prefix"]
+        self.suffix = Glyphs.defaults[key % "suffix"]
+        self.cutoff_score = Glyphs.defaults[key % "cutoff_score"]
 
     def showWindow_(self, sender):
         """Do something like show a window"""
